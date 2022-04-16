3c480f2540cadd31815c0643d12df55a726722de
David-Bhk <davidbihako4@gmail.com>
2022-04-16 21:12:01 +0200

    first commit, qr scanner

---
+++ app/src/main/java/com/example/codeqr/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.codeqr;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class MainActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        TextView tv_textView = findViewById(R.id.tv_textView);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv_textView.setText(result.getText());
+                    }
+                });
+
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+    protected void onResume() {
+
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+    protected void onPause(){
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+}
